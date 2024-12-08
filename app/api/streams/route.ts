aae702ef2126d1ed1c8da114cb7d85ef46389949
Deepak Kumar Sinha <94794077+sinhadeepak1115@users.noreply.github.com>
2024-12-08 23:44:16 +0530

    commit add stream route

--- app/api/streams/route.ts
+++ app/api/streams/route.ts
@@ -1,9 +1,11 @@
 import { prismaClient } from "@/lib/db";
+import e from "express";
 import { NextRequest, NextResponse } from "next/server";
 import { z } from "zod";
 
 export const YT_REGEX =
   /^(?:https?:\/\/)?(?:www\.)?(?:m\.)?(?:youtube\.com\/(?:watch\?(?!.*\blist=)(?:.*&)?v=|embed\/|v\/)|youtu\.be\/)([a-zA-Z0-9_-]{11})(?:[?&]\S+)?$/;
+
 const CreateStreamSchema = z.object({
   creatorId: z.string(),
   url: z.string(),
@@ -11,8 +13,14 @@ const CreateStreamSchema = z.object({
 
 export async function POST(req: NextRequest) {
   try {
-    const data = CreateStreamSchema.parse(await req.json());
-    const isYt = YT_REGEX.test(data.url);
+    const body = await req.json();
+
+    if (!body || typeof body !== "object") {
+      throw new Error("Request body is null or not an object");
+    }
+    const data = CreateStreamSchema.parse(body);
+    console.log(data);
+    const isYt = data.url.match(YT_REGEX);
     if (!isYt) {
       return NextResponse.json(
         {
@@ -21,22 +29,48 @@ export async function POST(req: NextRequest) {
         { status: 400 },
       );
     }
-    const extractedId = data.url.split("?v=")[1];
-    prismaClient.stream.create({
+
+    const extractedUrl = body.url.split("?v=")[1];
+
+    const stream = await prismaClient.stream.create({
       data: {
         userId: data.creatorId,
         url: data.url,
-        extractedId,
+        extractedUrl,
         type: "YouTube",
       },
     });
-    console.log(data);
-  } catch (error) {
+    console.log(stream);
     return NextResponse.json(
-      {
-        message: "Invalid data",
-      },
-      { status: 400 },
+      { message: "created the stream" },
+      { status: 200 },
+    );
+  } catch (error) {
+    if (error instanceof z.ZodError) {
+      return NextResponse.json(
+        {
+          message: "Validation Error",
+          errors: error.errors,
+        },
+        { status: 400 },
+      );
+    }
+    return NextResponse.json(
+      { message: "Internal Server Error", errors: error },
+      { status: 500 },
     );
   }
+}
+
+export async function GET(req: NextRequest) {
+  const creatorId = req.nextUrl.searchParams.get("creatorId");
+  const streams = await prismaClient.stream.findMany({
+    where: {
+      userId: creatorId ?? "",
+    },
+  });
+
+  return NextResponse.json({
+    streams,
+  });
 }
--- lib/auth.ts
+++ lib/auth.ts
@@ -1,5 +1,6 @@
 import NextAuth from "next-auth";
 import GoogleProvider from "next-auth/providers/google";
+import { prismaClient } from "./db";
 
 export const authOptions = {
   providers: [
@@ -8,6 +9,27 @@ export const authOptions = {
       clientSecret: process.env.GOOGLE_CLIENT_SECRET || "",
     }),
   ],
+  callbacks: {
+    async signIn(params) {
+      if (!params.user.email) {
+        return false;
+      }
+      try {
+        const user = await prismaClient.user.create({
+          data: {
+            email: params.user.email,
+            name: params.user.name,
+            image: params.user.image,
+            provider: "GOOGLE",
+          },
+        });
+        console.log(user);
+      } catch (e) {
+        console.error(e);
+      }
+      return true;
+    },
+  },
 };
 
 export default NextAuth(authOptions);
